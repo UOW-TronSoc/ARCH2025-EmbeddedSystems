f9b4e670586cf51a0786c9891ae9eaf82f24b0f9
Pierce O'Connor <74575565+Puncture-source@users.noreply.github.com>
2025-01-13 22:52:06 +1100

    add rfid code

--- ArmControl/ArmControl.ino
+++ ArmControl/ArmControl.ino
@@ -1,9 +1,39 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
 void setup() {
   // put your setup code here, to run once:
+  float slidePot = 0.0; // this is for the wrist pot
+  float lightread = 0.0; // this is for the 
+
+  Serial.begin(9600);
+  SPI.begin(); // init SPI bus
+  rfid.PCD_Init(); // init MFRC522
+
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (rfid.PICC_IsNewCardPresent()) { // new tag is available
+    if (rfid.PICC_ReadCardSerial()) { // NUID has been read
+      MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+      //Serial.print("RFID/NFC Tag Type: ");
+      //Serial.println(rfid.PICC_GetTypeName(piccType));
+
+      // print NUID in Serial Monitor in the hex format
+      Serial.print("UID:");
+      for (int i = 0; i < rfid.uid.size; i++) {
+        Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
+        Serial.print(rfid.uid.uidByte[i], HEX);
+      }
+      Serial.println();
+
+      rfid.PICC_HaltA(); // halt PICC
+      rfid.PCD_StopCrypto1(); // stop encryption on PCD
+    }
+  }
 
 }
