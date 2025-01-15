9319560e17b0bffc9a6230cd77c9e6fe1c301cff
Pierce O'Connor <74575565+Puncture-source@users.noreply.github.com>
2025-01-16 00:30:18 +1100

    comments and instructions

--- ArmControl/ArmControl.ino
+++ ArmControl/ArmControl.ino
@@ -1,7 +1,7 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define MaxGrasp 100
+#define MaxGrasp 100 // this needs to be changed once the max grasp is measured
 #define MinGrasp 0
 #define MaxValue 4096
 #define MinValue 0
@@ -9,10 +9,9 @@
 MFRC522 rfid(SS_PIN, RST_PIN);
 
 void setup() {
-  // put your setup code here, to run once:
   const int potPin = 34// placeholder pin number for now // this is for the wrist pot
   int potValue = 0;
-  int grasp = 0;
+  int grasp = 0; // we could change this to a float if we really cared about that level of precision
 
 
   Serial.begin(115200);
@@ -23,6 +22,7 @@ void setup() {
 }
 
 void loop() {
+  // RFID stuff
   if (rfid.PICC_IsNewCardPresent()) { // new tag is available
     if (rfid.PICC_ReadCardSerial()) { // NUID has been read
       MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
@@ -36,17 +36,21 @@ void loop() {
         Serial.print(rfid.uid.uidByte[i], HEX);
       }
       Serial.println();
+      // this printing will have to be replaced
 
       rfid.PICC_HaltA(); // halt PICC
       rfid.PCD_StopCrypto1(); // stop encryption on PCD
     }
   }
 
-  potValue = analogRead(potPin);
-  grasp = (MaxGrasp - MinGrasp) ((potValue-MinValue)/(MaxValue-MinValue)) + MinGrasp;
+
+// Linear Sliding potentiometer stuff
+  potValue = analogRead(potPin); // read the value of the pot slider
+  grasp = (MaxGrasp - MinGrasp) ((potValue-MinValue)/(MaxValue-MinValue)) + MinGrasp; // this maps the input to the range of the 
+  //hand so we can just output it as a distance
   Serial.print("Grasp size: ");
   Serial.print(grasp);
-  Serial.println("%");
+  //Serial.println("mm");
 
   delay(500);
 }
